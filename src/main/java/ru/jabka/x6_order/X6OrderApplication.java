f7b0487a41792987cc9f5069be7d8dc0a3095735
Олег <ole53@yandex.ru>
2025-03-29 23:13:23 +0300

    init

---
+++ src/main/java/ru/jabka/x6_order/X6OrderApplication.java
@@ -0,0 +1,13 @@
+package ru.jabka.x6_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X6OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(X6OrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jabka/x6_order/X6OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jabka.x6_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X6OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
